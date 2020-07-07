0477a98754c85d2b559054252ad14a93930535eb
Oriel Banne <orielbanne@gmail.com>
2020-07-07 20:15:45 +0300

    older rev working now - pushed. results are not reaching expected accuracy

--- train/__init__.py
+++ train/__init__.py
@@ -1,6 +1,7 @@
 
 #from .train_loop import train_loop
 #from .cluster import cluster
+from train import parameters
 
 # __all__ = ['train_loop', 'cluster']
-__all__ = ['old_train_loop', 'parameters2', 'cluster']
+__all__ = ['old_train_loop', 'parameters', 'cluster']
--- train/old_train_loop.py
+++ train/old_train_loop.py
@@ -8,25 +8,25 @@ from datasets.experiments import get_experiment_modalities_params, experiments_i
 from model import PlantFeatureExtractor as FeatureExtractor
 
 from train.utils import get_checkpoint_name, get_used_modalities, get_levels_kernel
-from train import parameters2  # importing all parameters
+from train import parameters  # importing all parameters
 
 
 # define test config
 class TestConfig:
-    def __init__(self, use_checkpoints, checkpoint_name, epochs, batch_size, domain_adapt_lr, device, dataset,
-                 train_set, test_set, train_loader, feat_ext, label_cls, plant_cls, criterion, label_opt, plant_opt,
-                 ext_opt, best_loss, loss_delta, return_epochs):
-        self.use_checkpoints = use_checkpoints
+    def __init__(self, use_checkpoints, checkpoint_name, epochs, batch_size, domain_adapt_lr, device,
+                 dataset, train_set, test_set, train_loader, feat_ext, label_cls, plant_cls, criterion,
+                 label_opt, plant_opt, ext_opt, best_loss, loss_delta, return_epochs):
+        self.use_checkpoints = parameters.use_checkpoints
         self.checkpoint_name = checkpoint_name
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.domain_adapt_lr = domain_adapt_lr
+        self.epochs = parameters.epochs
+        self.batch_size = parameters.batch_size
+        self.domain_adapt_lr = parameters.domain_adapt_lr
         self.device = device
         self.dataset = dataset
         self.train_set = train_set
         self.test_set = test_set
         self.train_loader = train_loader
-        self.epochs = epochs
+        self.epochs = parameters.epochs
         self.feat_ext = feat_ext
         self.label_cls = label_cls
         self.plant_cls = plant_cls
@@ -35,8 +35,8 @@ class TestConfig:
         self.plant_opt = plant_opt
         self.ext_opt = ext_opt
         self.best_loss = best_loss
-        self.loss_delta = loss_delta
-        self.return_epochs = return_epochs
+        self.loss_delta = parameters.loss_delta
+        self.return_epochs = parameters.return_epochs
         self.epochs_without_improvement = 0
 
 
@@ -176,29 +176,30 @@ def restore_checkpoint(test_config: TestConfig):
 
 
 def main():
-    checkpoint_name = get_checkpoint_name(experiment, excluded_modalities)
+    checkpoint_name = get_checkpoint_name(parameters.experiment, parameters.excluded_modalities)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    curr_experiment = experiments_info[experiment]
-    modalities = get_experiment_modalities_params(curr_experiment, lwir_skip, lwir_max_len, vir_max_len,color_max_len)
-    used_modalities = get_used_modalities(modalities, excluded_modalities)
-    print(experiment_path)
+    curr_experiment = experiments_info[parameters.experiment]
+    modalities = get_experiment_modalities_params(curr_experiment, parameters.lwir_skip, parameters.lwir_max_len,
+                                                  parameters.vir_max_len, parameters.color_max_len)
+    used_modalities = get_used_modalities(modalities, parameters.excluded_modalities)
+    print(parameters.experiment_path)
 
-    if experiment_path is None:
-        experiment_path = experiment
+    if parameters.experiment_path is None:
+        experiment_path = parameters.experiment
     else:
-        experiment_path = experiment_path
+        experiment_path = parameters.experiment_path
 
     end_date = curr_experiment.end_date
-    if num_days is not None:
-        end_date = curr_experiment.start_date + timedelta(days=num_days-1)
+    if parameters.num_days is not None:
+        end_date = curr_experiment.start_date + timedelta(days=parameters.num_days - 1)
 
-    dataset = Modalities(experiment_path, experiment, split_cycle=split_cycle,
+    dataset = Modalities(experiment_path, parameters.experiment, split_cycle=parameters.split_cycle,
                          start_date=curr_experiment.start_date, end_date=end_date, **used_modalities)
 
-    train_set, test_set = ModalitiesSubset.random_split(dataset, train_ratio)
-    train_loader = data.DataLoader(train_set, batch_size=batch_size, num_workers=2, shuffle=True)
+    train_set, test_set = ModalitiesSubset.random_split(dataset, parameters.train_ratio)
+    train_loader = data.DataLoader(train_set, batch_size=parameters.batch_size, num_workers=2, shuffle=True)
 
     feat_extractor_params = dict()
     for mod in used_modalities.keys():
@@ -209,33 +210,30 @@ def main():
         }
 
     feat_ext = FeatureExtractor(**feat_extractor_params).to(device)
-    label_cls = nn.Sequential(nn.ReLU(), nn.Linear(512, len(classes[experiment]))).to(device)
+    label_cls = nn.Sequential(nn.ReLU(), nn.Linear(512, len(classes[parameters.experiment]))).to(device)
     plant_cls = nn.Sequential(nn.ReLU(), nn.Linear(512, train_set.num_plants)).to(device)
 
     criterion = nn.CrossEntropyLoss(reduction='sum').to(device)
 
-    label_opt = optim.SGD(label_cls.parameters(), lr=label_lr, weight_decay=1e-3)
-    plant_opt = optim.SGD(plant_cls.parameters(), lr=plant_lr, weight_decay=1e-3)
-    ext_opt = optim.SGD(feat_ext.parameters(), lr=extractor_lr, weight_decay=1e-3)
+    label_opt = optim.SGD(label_cls.parameters(), lr=parameters.label_lr, weight_decay=1e-3)
+    plant_opt = optim.SGD(plant_cls.parameters(), lr=parameters.plant_lr, weight_decay=1e-3)
+    ext_opt = optim.SGD(feat_ext.parameters(), lr=parameters.extractor_lr, weight_decay=1e-3)
 
     best_loss = float('inf')
 
-    test_config = TestConfig(use_checkpoints, checkpoint_name, epochs, batch_size, domain_adapt_lr, device,
-                             dataset, train_set, test_set, train_loader, feat_ext, label_cls, plant_cls, criterion,
-                             label_opt, plant_opt, ext_opt, best_loss, loss_delta, return_epochs)
+    test_config = TestConfig(parameters.use_checkpoints, checkpoint_name, parameters.epochs, parameters.batch_size,
+                             parameters.domain_adapt_lr, device, dataset, train_set, test_set, train_loader, feat_ext,
+                             label_cls, plant_cls, criterion, label_opt, plant_opt, ext_opt, best_loss,
+                             parameters.loss_delta, parameters.return_epochs)
 
-    if load_checkpoint:
+    if parameters.load_checkpoint:
         restore_checkpoint(test_config)
 
     train_loop(test_config)
 
-    if use_checkpoints:
+    if parameters.use_checkpoints:
         restore_checkpoint(test_config)
 
 
-
 if __name__ == '__main__':
-    parameters2.init()
-    print(experiment)
     main()
-
